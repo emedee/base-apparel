b8be1998a4a87fb9d7617e20b19e57de8e79cc19
emedee <emediongini@gmail.com>
2020-03-10 03:59:04 -1200

    'firebase'

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 let submitButton = document.querySelector('.btn');
-let inputField = document.querySelector('#my-email');
+let emailField = document.querySelector('#my-email');
+let passField = document.querySelector('#my-password');
 let errorIcon = document.querySelector('.error-icon');
 let response = document.querySelector('.valid-email');
 
 
-function ValidateEmail(inputField){
+function ValidateEmail(user){
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(inputField.value.match(mailformat)){
+    if(emailField.value.match(mailformat) && passField.value !== ''){
         response.innerHTML = 'email added successfully!';
         response.style.color = 'rgb(150, 255, 140)'
         errorIcon.style.display = 'none';
-        inputField.style.border = '1px solid hsl(0, 36%, 70%)';
-        inputField.value = '';
+        emailField.style.border = '1px solid hsl(0, 36%, 70%)';
+        axios.post('https://base-apparel-12981.firebaseio.com/data.json', user)
+            .then(function(res){
+                console.log(res);
+            }, function(error){
+                console.log(error);
+            }, emailField.value = '', passField.value = '');
+    }else if(passField.value === ''){
+        response.innerHTML = 'Please add a password'
+        passField.style.border = '2px solid hsl(0, 93%, 68%)';
+        response.style.color = 'hsl(0, 93%, 68%)';
     }else{
-        inputField.style.border = '2px solid hsl(0, 93%, 68%)';
+        emailField.style.border = '2px solid hsl(0, 93%, 68%)';
         errorIcon.style.display = 'block';
         response.innerHTML = 'Please provide a valid email';
         response.style.color = 'hsl(0, 93%, 68%)';
     }
+   
 }
 
 submitButton.addEventListener('click', function(){
-    ValidateEmail(inputField);
+    var inputField = document.querySelector('#my-email').value;
+    var passwordField = document.querySelector('#my-password').value;
+    const user = {inputField, passwordField}
+    ValidateEmail(user);
 })
